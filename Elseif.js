f63fa2cf17574ac8283b96e1ccb6a3758b621f2e
Amit Sharma <sharmaamit09673@gmail.com>
2024-07-24 17:39:13 +0530

    Update Elseif.js

--- Elseif.js
+++ Elseif.js
@@ -1,52 +1,5 @@
-//Condition Statements
-// IF STATEMENT
-//voting
-/*let age=20;
-if(age>18){
-    console.log("Eligable for voting and license");
-}
-//mode
-let mode="dark";
-let color;
-if(mode==="dark")
-    {
-    color="black";
-}
-if(mode==="light")
-    {
-  color="white";
-}
-console.log(color);
-
-
-//IF-ELSE STATEMENT
-//mode
-let mode="light";
-let color;
-if(mode==="black"){
-    color="black";
-}else{
-    color="white";
-}
-console.log(color);
-
-//voting
-let age=15;
-if(age>18){
-    console.log("You Can Vote");
-}else{
-    console.log("You Cannot Vote");
-}
-
-//odd/even
-let no=3;
-if(no%2==0){
-    console.log(no," is Even");
-}else{
-    console.log(no," is Odd");
-}
-*/
 //ELSE-IF STATEMENT
+//mode
 let mode="dark";
 let color;
 if(mode==="dark"){
@@ -60,6 +13,7 @@ if(mode==="dark"){
 }
 console.log(color);
 
+//age
 let age=56;
 if(age>19){
     console.log("junior");
@@ -68,12 +22,3 @@ if(age>19){
  }else{
     console.log("Middle");
  }
-
-//---ternary operators----
- let m=19;
- let r=m>18?"adult":"not adult";
-console.log(r);
-
-
- m>18?console.log("adult"):console.log("not adult");
-
