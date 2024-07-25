b3b3813dace90bb02ea3dbe46b8c43dbc3ae4ba0
Amit Sharma <sharmaamit09673@gmail.com>
2024-07-25 17:06:05 +0530

    Add files via upload

---
+++ specialloop.js
@@ -0,0 +1,26 @@
+//special-loop
+//for of loop to loop on string,array 
+//printing ahmedabad
+let str="Ahmedabad";
+
+for(let i of str){
+    console.log("i:",i);
+}
+
+//print javascript
+let str1="Javascript";
+
+for(let i of str1){
+    console.log("i:",i);
+}
+
+//for in loop to loon on object,array
+let student={
+    fname : "amit",
+    age:19,
+    cgpa:8.1
+};
+
+for (let i in student){
+    console.log(i);
+}
