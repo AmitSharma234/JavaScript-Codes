0ad07daf36c2d9c180db028c404563b03c77ae17
Amit Sharma <sharmaamit09673@gmail.com>
2024-07-26 17:06:04 +0530

    Add files via upload

---
+++ String.js
@@ -0,0 +1,20 @@
+//all about strings in javascript
+let st="amit";
+console.log(st);
+console.log(st.length);
+console.log(st[1]);
+
+let obj={
+    item:'pen',
+    price:20
+};
+console.log('The Cost Of',obj.item,'is',obj.price,'rupess');
+//let up=this is product name : ${ obj .item} and its price : ${obj.price} rupess;
+//console.log(up);
+
+//esape character
+//   \n - new line
+//  \t - tab
+
+console.log("Amit\nSharma");
+console.log("Amit\tSharma");
---
+++ stringmethod.js
@@ -0,0 +1,31 @@
+//string methods
+//string convert into uppercase
+let s="devansh jain";
+s=s.toUpperCase();
+console.log(s);
+//string convert into lowercase
+s=s.toLowerCase();
+console.log(s);
+//string start and end spacing trim
+let s1="   karan     sehgal     ";
+s1=s1.trim();
+console.log(s1);
+
+//string slice
+let str="0123456";
+console.log(str.slice(1,5));   
+
+//string concatnation
+let str1="amit";
+let str2="sharma";
+let m1=str1.concat(str2);
+console.log(m1);
+
+//string replace
+let str4="hero";
+console.log(str.replace("h","s"));
+
+//searching particular character
+let str5="isia";
+console.log(str.charAt(0));
+
